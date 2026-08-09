7a0a32f21e29cce6c12ea108119585380a835b7e
zenari7777-web <zenari7777@gmail.com>
2026-08-09 17:56:11 +0400

    Create BootReceiver.kt

---
+++ app/src/main/java/com/dogmind/screenrecorder/BootReceiver.kt
@@ -0,0 +1,18 @@
+package com.dogmind.screenrecorder
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val action = intent.action
+        if (action == Intent.ACTION_BOOT_COMPLETED || action == "android.intent.action.QUICKBOOT_POWERON") {
+            val i = Intent(context, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            }
+            context.startActivity(i)
+        }
+    }
+}
